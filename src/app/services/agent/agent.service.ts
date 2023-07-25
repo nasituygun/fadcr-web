f52ed43e63f07b29b44115cb98623c475a68692d
Nasit UYGUN <nasituygun@gmail.com>
2023-07-25 14:31:54 +0300

    Update agent.service.ts

--- src/app/services/agent/agent.service.ts
+++ src/app/services/agent/agent.service.ts
@@ -10,7 +10,7 @@ export class AgentService {
 
   constructor(private httpClient: HttpClient) { }
 
-  private apiBaseUrl: string = "https://rocky-eyrie-41900-8f3b64454724.herokuapp.com/api/v1/agent/"
+  private apiBaseUrl: string = "http://3.87.189.110:8089/api/v1/agent/"
 
   
   getAllAgents(): Observable<ProjectInformation[]> {
